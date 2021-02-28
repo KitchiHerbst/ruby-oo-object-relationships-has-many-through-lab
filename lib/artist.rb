8830c0d9893b25606adf4e0838f56c131fde1dad
Triston <tristonherbst97@gmail.com>
2021-02-28 16:47:17 -0600

    commit tree b12f7b7fc1425004f954735b79493266c9762e90

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,38 @@
 
+
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name=name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select{|song| self == song.artist}
+    end
+
+    def new_song(name,genre)
+       Song.new(name,self,genre) 
+    end
+
+    def genres
+        # array = []
+        # songs.each do |song|
+        #     array << song.genre
+        # end
+        # array
+        songs.map do |song|
+            song.genre
+        end
+    end
+
+
+end 
